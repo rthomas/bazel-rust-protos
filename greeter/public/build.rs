a9d031eecd601dd705159103cab03a61660d9ec1
Ryan Thomas <ryan@ryant.org>
2022-10-18 15:43:49 +1100

    Add support for aarch64 (i.e. Apple M2).
    This also increases the terrible protoc hack as it's in a different location on macos.

--- greeter/public/build.rs
+++ greeter/public/build.rs
@@ -2,6 +2,9 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     let protos = vec!["greeter.proto"];
 
     // Terrible terrible hack...
+    #[cfg(target_os = "macos")]
+    std::env::set_var("PROTOC", "/opt/homebrew/bin/protoc");
+    #[cfg(not(target_os = "macos"))]
     std::env::set_var("PROTOC", "/usr/bin/protoc");
 
     for proto in protos {
